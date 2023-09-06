088142131570c0a6e80d8800b2bc008975adedad
kgsong <kyunggeun.song@seavantage.com>
2023-09-06 16:52:00 +0900

    ci: technical commit

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -3,5 +3,12 @@ import io.kotest.matchers.shouldBe
 
 class CalculatorTest : FreeSpec({
 
+    val calculator = Calculator()
+
+    "addition" - {
+        "should add two numbers" {
+            calculator.sum(2, 4) shouldBe 6
+        }
+    }
 
 })
