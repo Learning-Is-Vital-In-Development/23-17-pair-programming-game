09aededc427b3d6342dd3bb51130391796e3b67a
이건창 <geonc123@estsoft.com>
2023-10-09 20:56:31 +0900

    refact : 테스트 검증 로직 추가

--- src/main/kotlin/money/Amount.kt
+++ src/main/kotlin/money/Amount.kt
@@ -1,11 +1,17 @@
 package money
 
+import java.lang.IllegalArgumentException
+
 class Amount(val stringAmount: String) {
     private val integers = '0'..'9'
 
     init {
+        if (stringAmount.isBlank()) {
+            throw IllegalArgumentException("값이 비어있습니다.")
+        }
+
         if (!isInteger(stringAmount)) {
-            throw java.lang.IllegalArgumentException("입력한 $stringAmount 값은 숫자여야 합니다.")
+            throw IllegalArgumentException("입력한 $stringAmount 값은 숫자여야 합니다.")
         }
     }
 
--- src/test/kotlin/money/AmountTest.kt
+++ src/test/kotlin/money/AmountTest.kt
@@ -12,7 +12,7 @@ class AmountTest : FunSpec({
         "4000"
     ).forEach {
         test("금액은 입력받으면 BigDecimal 자료형을 반환한다. 입력받은 금액 : $it") {
-            Amount(it).stringAmount.toBigDecimal() shouldBe BigDecimal(it)
+            Amount(it).getBigDecimal() shouldBe BigDecimal(it)
         }
     }
 
@@ -21,7 +21,8 @@ class AmountTest : FunSpec({
         "유효하지 않은 숫자",
         "3k34j4",
         "k3333",
-        "3333k"
+        "3333k",
+        ""
     ).forEach {
         test("잘못된 문자열을 입력받으면 예외가 발생한다. 입력받은 문자 : $it") {
             shouldThrowExactly<IllegalArgumentException> {
