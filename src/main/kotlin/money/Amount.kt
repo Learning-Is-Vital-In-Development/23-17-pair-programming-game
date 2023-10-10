c5b329374f7206e1280f0a0dfa7e43e648c0849c
이건창 <geonc123@estsoft.com>
2023-10-10 20:39:15 +0900

    refact : Amount 코드 수정

--- src/main/kotlin/money/Amount.kt
+++ src/main/kotlin/money/Amount.kt
@@ -1,13 +1,11 @@
 package money
 
-import java.lang.IllegalArgumentException
-
 class Amount(private val stringAmount: String) {
     private val integers = '0'..'9'
 
     init {
         require(stringAmount.isNotBlank()) { "값이 비어있습니다." }
-        require(isInteger(stringAmount)) { "입력한 $stringAmount 값은 숫자여야 합니다." }
+        require(stringAmount.isInt()) { "입력한 $stringAmount 값은 숫자여야 합니다." }
     }
 
     fun getBigDecimal() = stringAmount.toBigDecimal()
