a59771011c21b58833bbddcf40acdbcd000ba5cd
doxxx <doxxx93@gmail.com>
2023-10-11 01:23:46 +0000

    fix: modify LottoNumber to data class

--- src/main/kotlin/model/lotto/LottoNumber.kt
+++ src/main/kotlin/model/lotto/LottoNumber.kt
@@ -1,6 +1,6 @@
 package model.lotto
 
-class LottoNumber(val value: Int) {
+data class LottoNumber(val value: Int) {
     init {
         require(value in 1..45) { "로또 번호는 1에서 45 사이여야 합니다." }
     }
