11a0ae32d0dc86a1449562a8a633b788ae5fae63
이건창 <geonc123@estsoft.com>
2023-10-18 16:09:22 +0900

    refactor : 린트 수정

--- src/main/kotlin/view/Number.kt
+++ src/main/kotlin/view/Number.kt
@@ -4,7 +4,7 @@ class Number(private val stringNumber: String) {
     private val integers = '0'..'9'
 
     init {
-        require(stringNumber.isNotBlank()) { "번호가 비어있을 수 없습니다."}
+        require(stringNumber.isNotBlank()) { "번호가 비어있을 수 없습니다." }
         require(stringNumber.isInt()) { INVALID_NUMBER_FORMAT_MESSAGE }
     }
 
