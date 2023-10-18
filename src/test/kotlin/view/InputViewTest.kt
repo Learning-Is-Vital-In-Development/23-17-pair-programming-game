1a931fbfa8e401cf38326dd69d4eb1195c3c4bde
이건창 <geonc123@estsoft.com>
2023-10-18 15:58:25 +0900

    test : InputViewTest 테스트 작성

---
+++ src/test/kotlin/view/InputViewTest.kt
@@ -0,0 +1,24 @@
+package view
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldContainAll
+import java.io.ByteArrayInputStream
+
+private const val DELIMITER = " "
+
+class InputViewTest : BehaviorSpec(
+    {
+        given("사용자의 입력을 받을 때") {
+            val inputView = InputView()
+            val strings = "12 32 2 1 4 5"
+            val expected = strings.split(DELIMITER).map { it.toInt() }
+            `when`("$strings 번호를 입력하면") {
+                System.setIn(ByteArrayInputStream(strings.toByteArray()))
+                val actual = inputView.requestLottoNumber()
+                then("$expected 리스트가 반환된다.") {
+                    actual shouldContainAll expected
+                }
+            }
+        }
+    },
+)
