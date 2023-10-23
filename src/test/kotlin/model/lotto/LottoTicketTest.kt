5f54ccd9d4229abc6672abbafe91fc24f71ae09b
doxxx <doxxx93@gmail.com>
2023-10-23 14:59:06 +0900

    test: Add LottoTicket unit tests

---
+++ src/test/kotlin/model/lotto/LottoTicketTest.kt
@@ -0,0 +1,26 @@
+package model.lotto
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class LottoTicketTest : FreeSpec({
+    "LottoTicket 생성" - {
+        "주어진 LottoNumbers 와 TicketType 을 가진 LottoTicket 을 반환" {
+            val numbers = LottoNumbers.from(listOf(1, 2, 3, 4, 5, 6))
+            val ticketType = TicketType.Manual
+            val ticket = LottoTicket.of(numbers, ticketType)
+
+            ticket.numbers shouldBe numbers
+            ticket.type shouldBe ticketType
+        }
+    }
+
+    "toString 메소드" - {
+        "LottoTicket 의 numbers 필드를 문자열화 하여 반환" {
+            val numbers = LottoNumbers.from(listOf(1, 2, 3, 4, 5, 6))
+            val ticket = LottoTicket.of(numbers, TicketType.Manual)
+
+            ticket.toString() shouldBe numbers.toString()
+        }
+    }
+})
