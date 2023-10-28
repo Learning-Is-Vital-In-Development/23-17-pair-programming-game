de3535145a4c1c96de075bef2258570a22e39cfd
haril song <songkg7@gmail.com>
2023-10-28 13:48:43 +0900

    feat: input winningNumber

--- src/main/kotlin/controller/LottoApp.kt
+++ src/main/kotlin/controller/LottoApp.kt
@@ -13,12 +13,22 @@ class LottoApp(private val inputView: InputView, private val outputView: OutputV
     fun run() {
         val money = validInputView({ inputCapital() }) { outputView.printMessage(it) }
         val lottoCount: LottoCount = validInputView({ requestManualCount(money) }) { outputView.printMessage(it) }
-        validInputView(
-            {
-                val lottoTickets = generateLottoTickets(lottoCount)
-                outputView.printLottoTickets(lottoTickets)
-            },
-        ) { outputView.printMessage(it) }
+        val lottoTickets = validInputView({ generateLottoTickets(lottoCount) }) { outputView.printMessage(it) }
+        outputView.printLottoTickets(lottoTickets)
+
+        // 당첨 번호 입력
+        val winningNumbers = validInputView({ inputWinningNumber() }) { outputView.printMessage(it) }
+
+        // 당첨 계산
+        // GameResult()
+
+        // 결과 출력
+        // print(gameResult)
+    }
+
+    private fun inputWinningNumber(): LottoNumbers {
+        outputView.requestWinningNumber()
+        return LottoNumbers.from(inputView.requestLottoNumber())
     }
 
     private fun inputCapital(): Money {
@@ -32,6 +42,7 @@ class LottoApp(private val inputView: InputView, private val outputView: OutputV
         return LottoCount.of(manualCount, money)
     }
 
+    // FIXME: 테스트 용이성이나 객체지향 관점에서 Ticket 은 LottoTicket 이 직접 생성해줘야하지 않을까요?
     private fun generateLottoTickets(lottoCount: LottoCount): List<LottoTicket> {
         val manualTicketNumbers = requestManualLottoNumbers(lottoCount.manualCount)
         val manualTickets = LottoTicketGenerator.generate(manualTicketNumbers)
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -6,6 +6,7 @@ import model.lotto.TicketType
 internal const val INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
 internal const val INPUT_MANUAL_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요."
 internal const val INPUT_LOTTO_NUMBER_MESSAGE = "로또 번호를 입력해 주세요. (공백으로 구분)"
+internal const val INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요. (공백으로 구분)"
 
 class OutputView {
     fun requestCapital() {
@@ -36,5 +37,9 @@ class OutputView {
             println("자동으로 구매한 로또 번호")
             autoTickets.forEach { println(it.numbers) }
         }
+    }
+
+    fun requestWinningNumber() {
+        println(INPUT_WINNING_NUMBER)
     }
 }
